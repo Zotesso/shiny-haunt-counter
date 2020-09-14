cd273945d4940caa04f2f2d935270b0589de8291
Zotesso <siriothart@gmail.com>
2020-09-14 20:37:50 -0300

    Starting the Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './views/components/footer/footer.component';
+import { HeaderComponent } from './views/components/header/header.component';
+import { PokeHuntComponent } from './views/components/poke-hunt/poke-hunt.component';
+import { PokeSelectComponent } from './components/poke-select/poke-select.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    PokeHuntComponent,
+    PokeSelectComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
